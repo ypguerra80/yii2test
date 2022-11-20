9c937f7ea5b0761f6828106dd98de5dec8d578d2
ypguerra80 <ypguerra80@gmail.com>
2022-11-19 23:53:11 -0500

    Auth redirections

--- tasks_app/lib/main.dart
+++ tasks_app/lib/main.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:tasksapp/classes/user.dart';
+import 'package:tasksapp/views/error-screen.dart';
+import 'package:tasksapp/views/login.dart';
+import 'package:tasksapp/views/splash.dart';
 import 'package:tasksapp/views/tasks.dart';
+//TODO: Check for flutter_secure_storage configurations on different platforms.
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  _MyApp createState() => _MyApp();
+
+}
+
+class _MyApp extends State<MyApp> with TickerProviderStateMixin {
+
+  late Future<User> user;
+  final storage = const FlutterSecureStorage();
+
+  @override
+  void initState() {
+
+    user = _getSavedUser();
+
+    super.initState();
+  }
 
   // This widget is the root of your application.
   @override
@@ -14,18 +38,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const Tasks(title: 'Task Manager'),
+      home: FutureBuilder<User>(
+        future: user,
+        builder: (context, snapshot) {
+          if(snapshot.hasData && snapshot.requireData.token.isNotEmpty){
+            if(snapshot.requireData.name == ''){
+              return const Login();
+            } else {
+              return const Tasks();
+            }
+          }
+
+          return snapshot.hasError ? const ErrorScreen() : const Splash();
+        },
+      ),
     );
   }
+
+  Future<User> _getSavedUser() async {
+    String? userToken = await storage.read(key: 'user_token_key');
+    String token = userToken ?? '';
+
+    return User(token, 'name');
+  }
+
 }
---
+++ tasks_app/lib/views/error-screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ErrorScreen extends StatelessWidget {
+  const ErrorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Task Manager'),
+      ),
+      body: const Center(
+        child: Text('...Error'),
+      ),
+    );
+  }
+}
